755efb4b760b8630d23d8e2bbcf84e42bef48970
Kangning Huang <kh3657@nyu.edu>
2026-09-01 12:51:37 +0800

    Update three papers on the website and CV

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,3 +7,14 @@ export const SITE = {
   email: "kangning.huang@nyu.edu",
   googleScholarId: "s_domssAAAAJ",
 };
+
+export const LINKS = {
+  googleScholar:
+    "https://scholar.google.com/citations?user=s_domssAAAAJ&hl=en",
+  github: "https://github.com/kangning-huang",
+  twitter: "https://x.com/FytasoK60918",
+  substack: "https://kangninghuang.substack.com",
+  nyuFaculty:
+    "https://shanghai.nyu.edu/academics/faculty/directory/kangning-ken-huang",
+  lab: "https://knhuang.weebly.com",
+};
