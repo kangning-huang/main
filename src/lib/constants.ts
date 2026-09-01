31b9e177f52058c1b5faf8ec3e7756b80f844905
Kangning Huang <kh3657@nyu.edu>
2026-09-01 13:02:52 +0800

    Correct paper years: density 2025; flood paper accepted 2026

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,20 @@
-RETRY_SAME_ACTION
+export const SITE = {
+  name: "Kangning (Ken) Huang",
+  title: "Assistant Professor of Environmental Studies",
+  affiliation: "NYU Shanghai",
+  description:
+    "Building a sustainable and resilient urban future through research on urbanization, climate change, and environmental hazards.",
+  email: "kangning.huang@nyu.edu",
+  googleScholarId: "s_domssAAAAJ",
+};
+
+export const LINKS = {
+  googleScholar:
+    "https://scholar.google.com/citations?user=s_domssAAAAJ&hl=en",
+  github: "https://github.com/kangning-huang",
+  twitter: "https://x.com/FytasoK60918",
+  substack: "https://kangninghuang.substack.com",
+  nyuFaculty:
+    "https://shanghai.nyu.edu/academics/faculty/directory/kangning-ken-huang",
+  lab: "https://knhuang.weebly.com",
+};
